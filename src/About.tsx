b62de6203ffe6c6c992a670a3688d6aec4f5f9ed
cai-mci <52683779+cai-mci@users.noreply.github.com>
2025-11-23 14:58:20 -0600

    deployment?

--- src/About.tsx
+++ src/About.tsx
@@ -6,6 +6,9 @@ export function About() {
             </div>
             <h1>Hi, I'm Caitlin</h1>
             <h2>Junior Computer Science Student at Texas A&M</h2>
+            <p>I’m passionate about building software that blends clear, efficient code with real-world impact, whether that’s optimizing backend systems, creating reliable robotics code, or leading teams to deliver meaningful community tech projects. 
+
+I thrive in collaborative, fast-paced environments where learning never stops and innovation drives purpose. My goal is to become a well-rounded software engineer who bridges technical depth with leadership and creativity.</p>
         </div>
     )
 }
